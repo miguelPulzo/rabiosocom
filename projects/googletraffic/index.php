c92702278d8bd5ba99af9d8d3c7add14344eb6ad
Miguel Angel G.R <miguel.garcia@pulzo.com>
2015-09-06 10:47:55 -0500

    ajuste

--- projects/googletraffic/index.php
+++ projects/googletraffic/index.php
@@ -21,7 +21,7 @@
   function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 13,
-      center: {lat: 34.04924594193164, lng: -118.24104309082031},
+      center: {lat: 4.7155781, lng: -74.0529183},
       disableDefaultUI: true
     });
 
